4720713d32450403c29d75ecf35e73d53bb67e4c
lordson-silver <lordsonochuko5@gmail.com>
2022-09-28 11:34:22 +0100

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import {useEffect} from "react";
+import MovieCard from "./MovieCard";
 import './App.css';
+// import SearchIcon from './search.svg';
+
+const API_URL = '';
+const query = 'Spiderman';
+const movie1 = {
+    "Title": "Amazing Spiderman",
+    "Year": "2012",
+    "imdbID": "tt12345",
+    "Type": "movie",
+    "Poster": "N/A"
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    };
+
+    useEffect(() => {
+        searchMovies({ query });
+    }, []);
+    return (
+        <div className="app">
+            <h1>Moovies</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for movies"
+                    value={query}
+                    onChange={() => { } } />
+                {/* <img
+                src={SearchIcon}
+                alt="Search Icon"
+                onClick={() => {}}
+            /> */}
+            </div>
+
+            <div className="container">
+                
+               <MovieCard movie1={movie1}/>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
